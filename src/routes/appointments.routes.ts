7b88cc2f343c3ad03aba49b831e6389f55470bf6
masprog2022 <masprog2022@gmail.com>
2021-10-13 19:01:35 +0100

    project initial

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+
+const appointmentsRouter = Router();
+
+// Rota GET
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+  return response.json(appointments);
+});
+
+// Rota POST
+appointmentsRouter.post('/', async (request, response) => {
+
+  try{
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider
+    });
+
+    return response.json(appointment);
+
+   }catch (err){
+     return response.status(400).json({error:  (err as Error).message})
+   }
+
+});
+
+export default appointmentsRouter;
