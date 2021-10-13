5cc88b19a85d2833eea48fafc7e8eecdd73767b9
masprog2022 <masprog2022@gmail.com>
2021-10-13 21:42:25 +0100

    relationship models

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -5,16 +5,25 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn
 
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
 
   @Column('timestamp with time zone')
